eae919b191fbb51738cbf41f693d71b81b06dfce
Alicia Tsai <aliciatsai@berkeley.edu>
2020-03-04 11:29:35 -0800

    collect data for a duration of time

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from threading import Timer
-from earthquake_data import get_earthquake_data
+from src.earthquake_data import get_earthquake_data
 
 today = datetime.today()
 tomorrow = today.replace(day=today.day+1, hour=1, minute=0, second=0, microsecond=0)
