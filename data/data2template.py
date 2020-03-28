42b73c2f3839568dad8f0d057bb818f6b04005b1
lcl940610 <35822135+lcl940610@users.noreply.github.com>
2020-03-28 12:38:31 -0700

    Add files via upload

--- data/data2template.py
+++ data/data2template.py
@@ -2,22 +2,38 @@
 import os
 import re
 import csv
+import pandas
 import calendar
+import pytz
+import pandas as pd
+from bs4 import BeautifulSoup
+from datetime import datetime
+from dateutil import tz
+from tzwhere import tzwhere
 
 def process(source_data, dest_file, index):
     """
     source_data: csv data file, with directory to the csv file.
     dest_file: txt template.
-    index: the index of event to be processed (starting at 1).
+    index: the index of event to be processed (starting at 0).
     """
-    with open("earthquakes_log.csv") as f:
-        reader = csv.reader(f)
-        mylist = list(reader)    
-    time = str(mylist[index][mylist[0].index('rupture_time')])
-    time = re.split('-| |:', time)
-    result = "On {} {}, {}, at approximately {}:{} UTC, ".format(calendar.month_name[int(time[1])], time[2], time[0], time[3], time[4]); 
-    result += "a magnitude {} earthquake, with a depth of {} km, ".format(mylist[index][mylist[0].index('magnitude')], mylist[index][mylist[0].index('depth')]);
-    location = mylist[index][mylist[0].index('place')];
+    df = pd.read_csv("earthquakes_log.csv")   
+    utctime = df.iloc[index]['rupture_time']
+    longitude = df.iloc[index]['longitude']
+    latitude = df.iloc[index]['latitude']
+
+    # UTC to local time (daylight save is also considered)
+    tzwherev = tzwhere.tzwhere()
+    timezone_str = tzwherev.tzNameAt(latitude, longitude) # Seville coordinates
+    from_zone = tz.gettz('UTC')
+    to_zone = tz.gettz(timezone_str)
+    utc = datetime.strptime(utctime, '%Y-%m-%d %H:%M:%S')
+    utc = utc.replace(tzinfo=from_zone)
+    localtime = utc.astimezone(to_zone)
+
+    result = "On {} {}, {}, at approximately {}:{} local time, ".format(calendar.month_name[localtime.month], localtime.day, localtime.year, localtime.hour, localtime.minute); 
+    result += "a magnitude {} earthquake, with a depth of {} km, ".format(df.iloc[index]['magnitude'], df.iloc[index]['depth']);
+    location = df.iloc[index]['place'];
     location = re.split('km | | of', location);
     direction = {"E": "East", "W": "West", "S": "South", "N": "North", "SE": "Southeast", "SW": "Southwest", "NE": "Northeast", "NW": "Northwest", "ESE": "East-Southeast", "WSW": "West-Southwest", "ENE": "East-Northeast", "WNW": "West-Northwest", "SSE": "South-Southeast", "SSW": "South-Southwest", "NNE": "North-Northeast", "NNW": "North-Northwest"}
     city = "";
@@ -25,15 +41,15 @@ def process(source_data, dest_file, index):
         city += location[i] + " ";
     city = city[:-1];
     result += "struck {} km {} of {}. ".format(location[0], direction[location[1]], city);
-    if (float(mylist[index][mylist[0].index('latitude')]) > 0):
-        latitude = "N";
+    if (float(latitude) > 0):
+        latitudeNS = "N";
     else:
-        latitude = "S";
-    if (float(mylist[index][mylist[0].index('longitude')]) > 0):
-        longitude = "E";
+        latitudeNS = "S";
+    if (float(longitude) > 0):
+        longitudeEW = "E";
     else:
-        longitude = "W";
-    result += "The coordinate of epicenter of the earthquake was {}°{}, {}°{}.".format(abs(float(mylist[index][mylist[0].index('latitude')])), latitude, abs(float(mylist[index][mylist[0].index('longitude')])), longitude);
+        longitudeEW = "W";
+    result += "The coordinate of epicenter of the earthquake was {}°{}, {}°{}.".format(abs(float(latitude)), latitudeNS, abs(float(longitude)), longitudeEW);
     result += "\n";
     text = open("sample.txt", "a");
     text.write(result);
